b4a12549a4f057902364b01676cfc4705965e633
Matthew Sunner <matt@mattsunner.com>
2020-12-23 21:08:01 -0600

    Rewrite Script Functionally
    Rewrite the script in a functional format and test output.

--- covid_pipeline/pipeline.py
+++ covid_pipeline/pipeline.py
@@ -9,55 +9,57 @@ import pandas as pd
 import numpy as np
 import sqlite3
 
-federal_api = 'https://api.covidtracking.com/v1/us/current.json'
-state_api = 'https://api.covidtracking.com/v1/states/current.json'
 
-federal_df = pd.read_json(federal_api)
-state_df = pd.read_json(state_api)
+def update_database(federal_api, state_api, conn):
+    """update_database: Method to update the database with new COVID-19 daily data
 
-# Update Date with Daily Values
-federal_df['date'] = pd.to_datetime(federal_df['date'].astype(str), format='%Y%m%d')
-state_df['date'] = pd.to_datetime(state_df['date'].astype(str), format='%Y%m%d')
+    Args:
+        federal_api (str): URL for the federal API 
+        state_api (str): URL for the state API
+        conn (connection): SQLite3 connection method call and database name information
+    """
+    conn = conn
 
-db_name = '../data/output_data/covidDB.db'
-conn = sqlite3.connect(db_name)
+    federal_df = pd.read_json(federal_api)
+    state_df = pd.read_json(state_api)
 
-federal_df.to_sql('federaldata', conn, index=False, if_exists='append')
-state_df.to_sql('statedata', conn, index=False, if_exists='append')
+    df_persistent_fed = pd.read_sql('SELECT * FROM federaldata', conn)
+    hash_key_fed = federal_df['hash'][0]
 
-conn.commit()
+    check_federal = df_persistent_fed['hash'][df_persistent_fed['hash'].str.contains(hash_key_fed)].count()
 
-# Check if Persistence Matches Current API
+    if check_federal == 0:
+        # Update Date Formatting
+        federal_df['date'] = pd.to_datetime(federal_df['date'].astype(str), format='%Y%m%d')
+        state_df['date'] = pd.to_datetime(state_df['date'].astype(str), format='%Y%m%d')
+        
+        federal_df.to_sql('federaldata', conn, index=False, if_exists='append')
+        state_df.to_sql('statedata', conn, index=False, if_exists='append')
 
-persistent_federal_df = pd.read_sql('SELECT * FROM federaldata', conn)
-persistent_state_df = pd.read_sql('SELECT * FROM statedata', conn)
+        conn.commit()
+        conn.close()
+    else:
+        print('Database is already up to date.')
 
-federal_full_api = 'https://api.covidtracking.com/v1/us/daily.json'
-state_full_api = 'https://api.covidtracking.com/v1/states/daily.json'
-
-federal_full_df = pd.read_json(federal_full_api)
-state_full_df = pd.read_json(state_full_api)
-
-
-conn.close()
-
-
-
-
-""" 
-Process: 
-- Load in new day values for federal/state statistics -- cleaned and ready for storage
-- Check if full federal/state datasets match the current database 
-- If they match, break and save the dataset
-- If they do not match, delete the persisted data and reload a new full dataset (This assumes that updates were made)
-- Disconnect from the database and save it
-"""
+        conn.close()
 
 
-# # Custom Script Methods
-# def silentremove(filename):
-#     try:
-#         os.remove(filename)
-#     except OSError as e:
-#         if e.errno != errno.ENOENT:
-#             raise
+def main():
+    federal_api = 'https://api.covidtracking.com/v1/us/current.json'
+    state_api = 'https://api.covidtracking.com/v1/states/current.json'
+    conn = sqlite3.connect('../data/output_data/covidDB.db')
+
+    update_database(federal_api, state_api, conn)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
